5abb5ccc33c23be9285711ab58100d2fca6580d5
Prakharper <158788969+Prakharper@users.noreply.github.com>
2025-03-11 20:09:20 -0400

    Actualizar grupo-unbanchat.js

--- plugins/grupo-unbanchat.js
+++ plugins/grupo-unbanchat.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
 if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🍭l🍬 *¡Este chat no está registrado!*', m, fake)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '🍭 *¡Vegeta-Bot no está baneada en este chat!*', m, fake)
+if (!chat.isBanned) return conn.reply(m.chat, '💐 *¡Bulma-Bot no está baneada en este chat!*', m, fake)
 chat.isBanned = false
-await conn.reply(m.chat, '🍬 *¡Vegeta-Bot ya fué desbaneada en este chat!*', m, fake)
+await conn.reply(m.chat, ',💐 *¡Bulma-Bot ya fué desbaneada en este chat!*', m, fake)
 }
 handler.help = ['unbanchat'];
 handler.tags = ['grupo'];
