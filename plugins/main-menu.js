70c47e92aa60794ff31883deb3abb776a8d1a287
Leo7370 <leonardo.arenas.ve@gmail.com>
2025-03-16 15:38:09 -0700

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -29,7 +29,7 @@ let mentionedJid = [who]
 let perfil = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://i.ibb.co/GtYw361/Yuki.jpg')
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 const vid = ['https://qu.ax/wpaNt.mp4'
-'https://qu.ax/wpaNt.mp4', 'https://qu.ax/wpaNt.mp4',]
+'https://qu.ax/wpaNt.mp4', 'https://qu.ax/wpaNt.mp4']
 
 let menu = `✦☆𝗦𝗛𝗔𝗗𝗢𝗪-𝗕𝗢𝗧-𝗠𝗗☆✦
 
