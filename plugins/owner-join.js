3e823a50a3415c60b1adff091689f1be3cbf24a5
Leo7370 <leonardo.arenas.ve@gmail.com>
2025-03-25 11:26:29 -0700

    Actualizar owner-join.js

--- plugins/owner-join.js
+++ plugins/owner-join.js
@@ -1,7 +1,7 @@
 let linkRegex = /https:\/\/chat\.whatsapp\.com\/([0-9A-Za-z]{20,24})/i;
 
 let handler = async (m, { conn, text, isOwner }) => {
-    if (!text) return m.reply('🍬 Debes enviar una invitacion para que *Vegeta - Bot* se una al grupo.');
+    if (!text) return m.reply('🍬 Debes enviar una invitacion para que *Shadow - Bot* se una al grupo.');
 
     let [_, code] = text.match(linkRegex) || [];
 
